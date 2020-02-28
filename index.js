448a372974f17bbd8326afa0e04c76ca10700eab
Kashif Ullah <kashif.ullah@mobilelive.ca>
2020-02-28 11:34:04 +0500

    update port

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to Student Information System REST API');
 });
 
-app.listen(3000, () => { console.log('listening at port 3000 ...')});
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`listening at port ${port} ...`)});
