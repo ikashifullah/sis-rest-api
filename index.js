e03495498a0736cf4d05f0296c66ea88d4add040
Kashif Ullah <kashif.ullah@mobilelive.ca>
2020-02-28 11:52:32 +0500

    add GET single courese

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ app.get('/', (req, res) => {
     res.send('Welcome to Student Information System REST API');
 });
 
+// Get Courses
 app.get('/api/courses', (req, res) => {
     res.send(courses);
+});
+
+// Get a single course
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with given ID is not found!');
+    res.send(course);
 });
 
 // PORT
