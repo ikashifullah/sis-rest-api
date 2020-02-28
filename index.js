7395d19698df1b0c849f1f847d074a803a26ec6e
Kashif Ullah <kashif.ullah@mobilelive.ca>
2020-02-28 12:05:52 +0500

    Add custom validation for name

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+    if (!req.body.name || req.body.name < 3) {
+        res.status(400).send('Name is required and should be at least 3 characters');
+        return;
+    }
     const course = {
         id: courses.courses.length + 1,
         name: req.body.name,
